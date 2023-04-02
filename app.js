5e0a798dd00313ea58eefb43b288e8394e64e5cf
sidiraos <raossidi@gmail.com>
2023-04-02 12:48:15 +0300

    V1.4

--- app.js
+++ app.js
@@ -24,24 +24,49 @@ async function generateRandomQuote(){
 // generate quote string
 generateRandomQuote()
 // show quote in html and verifiy the quote word by word equal user's keypress
+let scoreGame = 0;
+let previousLength = textArea.value.length;
+let scoreForQuote = 0;
 function showQuote(quote){
     console.log(quote)
+    console.log(quote.length)
+    textArea.setAttribute('maxlength' , `${quote.length}`)
+    handleApp(quote);
+}
+
+function addMarkDownAndScore(check , trueColor , wrongColor, parentEl , i) {
+    do {
+        if(check){
+            scoreGame++;
+            scoreForQuote++;
+            console.log("scoreGame" ,scoreGame)
+            console.log("Score for quote" ,scoreForQuote);
+            score.textContent = `Score : ${scoreGame}`;
+        }
+        parentEl.children[i].style.background = trueColor;
+        parentEl.children[i].classList.add(trueColor);
+        if(parentEl.children[i].classList.contains(wrongColor)){
+            parentEl.children[i].classList.remove(wrongColor);
+        }
+        check = false;
+    } while (check == true);
+}
+
+function handleApp(quote){
     for (let i = 0; i < quote.length; i++){
         const span = document.createElement('span');
         span.textContent = quote[i];
         quoteEl.appendChild(span);
     }
-    let scoreGame = 0;
-    let previousLength = textArea.value.length;
-    let check = false;
-    textArea.addEventListener('input' , (e)=>{
+        textArea.addEventListener('input' , (e)=>{
         let userInput = e.target.value;
         let i = userInput.length -1;
         let currentLength = e.target.value.length;
         if (currentLength < previousLength) {
             console.log("item deleted");
             if(quoteEl.children[i+1].classList.contains("green")) {
-                scoreGame--
+                scoreGame--;
+                scoreForQuote--;
                 score.textContent = `Score : ${scoreGame}`
             }
             quoteEl.children[i+1].style.background = "none";
@@ -49,31 +74,26 @@ function showQuote(quote){
         } else {
             if(userInput.length > 0) {
                 if(userInput.charAt(i) === quote.charAt(i)) {
-                    check = true;
-                    do {
-                        scoreGame++;
-                        score.textContent = `Score : ${scoreGame}`;
-                        quoteEl.children[i].style.background = "green";
-                        quoteEl.children[i].classList.add("green");
-                        if(quoteEl.children[i].classList.contains("red")){
-                            quoteEl.children[i].classList.remove("red");
-                        }
-                        check = false;
-                    } while (check == true);
-    
+                    addMarkDownAndScore(true , "green" , "red" , quoteEl , i)
                 } else {
-                    quoteEl.children[i].style.background = "red";
-                    quoteEl.children[i].classList.add("red");
-                    if(quoteEl.children[i].classList.contains("green")){
-                        quoteEl.children[i].classList.remove("green");
-                    }
+                    addMarkDownAndScore(false , "red" , "green" , quoteEl , i)
                 }
             } else {
-                scoreGame = 0;
+                scoreGame = scoreGame;
+                scoreForQuote = scoreForQuote;
                 score.textContent = `Score : ${scoreGame}`
             }
         }
 
         previousLength = currentLength;
+        if(scoreForQuote == quote.length){
+            quoteEl.textContent= "";
+            textArea.value = "";
+            previousLength = 0;
+            scoreForQuote = 0;
+            console.log("Score for quote at the end" ,scoreForQuote)
+            generateRandomQuote()
+        }
+
     })
 }
