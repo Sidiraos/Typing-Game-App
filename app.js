8fd13f7db2dec97f72a6f0d8383ee310785f1d3c
sidiraos <raossidi@gmail.com>
2023-04-02 10:32:06 +0300

    V1.3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const textArea = document.querySelector('#textArea');
 
 async function generateRandomQuote(){
     try {
-        const response = await fetch(APIEndpoint);
+        // const response = await fetch(APIEndpoint);
         if (response.ok){
             const data = await response.json();
             let quote = data.content;
@@ -31,22 +31,49 @@ function showQuote(quote){
         span.textContent = quote[i];
         quoteEl.appendChild(span);
     }
-
+    let scoreGame = 0;
+    let previousLength = textArea.value.length;
+    let check = false;
     textArea.addEventListener('input' , (e)=>{
         let userInput = e.target.value;
-        let i = userInput.length -1
-        if(userInput.length > 0) {
-            if(userInput.charAt(i) === quote.charAt(i)) {
-                console.log("checked")
-                console.log(quoteEl.children[i]);
-                quoteEl.children[i].style.background = "green";
-        
+        let i = userInput.length -1;
+        let currentLength = e.target.value.length;
+        if (currentLength < previousLength) {
+            console.log("item deleted");
+            if(quoteEl.children[i+1].classList.contains("green")) {
+                scoreGame--
+                score.textContent = `Score : ${scoreGame}`
+            }
+            quoteEl.children[i+1].style.background = "none";
+            quoteEl.children[i+1].className = "";
+        } else {
+            if(userInput.length > 0) {
+                if(userInput.charAt(i) === quote.charAt(i)) {
+                    check = true;
+                    do {
+                        scoreGame++;
+                        score.textContent = `Score : ${scoreGame}`;
+                        quoteEl.children[i].style.background = "green";
+                        quoteEl.children[i].classList.add("green");
+                        if(quoteEl.children[i].classList.contains("red")){
+                            quoteEl.children[i].classList.remove("red");
+                        }
+                        check = false;
+                    } while (check == true);
+    
+                } else {
+                    quoteEl.children[i].style.background = "red";
+                    quoteEl.children[i].classList.add("red");
+                    if(quoteEl.children[i].classList.contains("green")){
+                        quoteEl.children[i].classList.remove("green");
+                    }
+                }
             } else {
-                console.log("unchecked")
-                console.log(quoteEl.children[i]);
-                quoteEl.children[i].style.background = "red";
-
+                scoreGame = 0;
+                score.textContent = `Score : ${scoreGame}`
             }
         }
+
+        previousLength = currentLength;
     })
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,13 @@
     <b>V1.0</b>
     <li>first commit </li>
 </ul>
+<ul>
+    <b>V1.2</b>
+    <li>add input event & check word verification fuction</li>
+</ul>
+<ul>
+    <b>V1.3</b>
+    <li>fixed bug input event & check word verification fuction</li>
+    <li>check the score correctly</li>
+    <li>add function when backspace and handle bug</li>
+</ul>
